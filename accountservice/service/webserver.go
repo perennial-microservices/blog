1f05541b4bf3364c8af903d2fc47aeebc6a4c583
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2018-06-25 16:24:58 +0530

    Added Router

--- accountservice/service/webserver.go
+++ accountservice/service/webserver.go
@@ -7,6 +7,8 @@ import (
 
 func StartWebServer(port string) {
 	log.Println("Starting HTTP service at " + port)
+	r:= NewRouter()
+	http.Handle("/", r)
 	err := http.ListenAndServe(":" + port, nil)
 	if err != nil {
 		log.Println("An error occured starting HTTP listener at port " + port)
