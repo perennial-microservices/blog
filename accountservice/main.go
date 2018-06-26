9b2b8c4ab8747ff42584da3071e41e21f7ab3e23
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2018-06-26 11:47:06 +0530

    Dockerise

--- accountservice/main.go
+++ accountservice/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/perennial-microservices/blog/accountservice/service"
+	"github.com/perennial-microservices/blog/accountservice/dbclient"
 )
 
 var serviceName = "Account Service"
 
 func main() {
 	fmt.Printf("Starting %v\n", serviceName)
+	initializeBoltClient()
 	service.StartWebServer("6767")
+}
+
+func initializeBoltClient() {
+	service.DBClient = &dbclient.BoltClient{}
+	service.DBClient.OpenBoltDb()
+	service.DBClient.Seed()
+
 }
