82ef6343e4a8f86aef50172e01635caa51fab1f2
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2018-06-28 13:14:35 +0530

    Bug Fixes with Docker Healthcheck Service

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd accountservice;env GOOS=linux GARCH=amd64 CGO_ENABLED=0 go build -a -installsuffix cgo -o accountservice-linux-amd64;echo built `pwd`;cd ..
-cd healthcheck;env GOOS=linux GARCH=amd64 CGO_ENABLED=0 go build -a -installsuffix cgo -o healthcheck-linux-amd64;echo built `pwd`;cd ..
+cd accountservice;env GOOS=linux GARCH=amd64 CGO_ENABLED=0 go build -a -installsuffix cgo -o accountservice-linux-amd64;echo built `pwd`;cd ..;
+cd healthcheck;env GOOS=linux GARCH=amd64 CGO_ENABLED=0 go build -a -installsuffix cgo -o healthcheck-linux-amd64;echo built `pwd`;cd ..;
 
 cp healthcheck/healthcheck-linux-amd64 accountservice/
 
-docker build -t asahasrabuddh/account-service accountservice/
+docker build -t asahasrabuddh/account-service:1.0 accountservice/
 docker push asahasrabuddh/account-service
